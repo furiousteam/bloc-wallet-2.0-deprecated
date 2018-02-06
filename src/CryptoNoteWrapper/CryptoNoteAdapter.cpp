90c600c7aff25d21b13b59c9d4fb71a2c472b539
furiousteam <36113405+furiousteam@users.noreply.github.com>
2018-02-06 16:45:51 +0100

    Changed Ticker BKC to BLOC
    QString CryptoNoteAdapter::getCurrencyTicker() const {
      return "BLOC";

--- src/CryptoNoteWrapper/CryptoNoteAdapter.cpp
+++ src/CryptoNoteWrapper/CryptoNoteAdapter.cpp
@@ -107,7 +107,7 @@ bool CryptoNoteAdapter::isValidPaymentId(const QString& _paymentId) const {
 }
 
 QString CryptoNoteAdapter::getCurrencyTicker() const {
-  return "BKC";
+  return "BLOC";
 }
 
 quint64 CryptoNoteAdapter::getMinimalFee() const {
