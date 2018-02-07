f03959e94da7affae0e4a3f84ee9a9c74f3df1db
furiousteam <36113405+furiousteam@users.noreply.github.com>
2018-02-07 05:20:44 +0100

    Update ui_MainWindow.h

--- src/Gui/MainWindow/ui_MainWindow.h
+++ src/Gui/MainWindow/ui_MainWindow.h
@@ -637,7 +637,7 @@ public:
 #ifndef QT_NO_TOOLTIP
         m_changePasswordAction->setToolTip(QApplication::translate("MainWindow", "Change password", Q_NULLPTR));
 #endif // QT_NO_TOOLTIP
-        m_aboutIntensecoinAction->setText(QApplication::translate("MainWindow", "About Intensecoin", Q_NULLPTR));
+        m_aboutIntensecoinAction->setText(QApplication::translate("MainWindow", "About BlockChain-Coin", Q_NULLPTR));
         m_aboutQtAction->setText(QApplication::translate("MainWindow", "About Qt", Q_NULLPTR));
         m_backupWalletAction->setText(QApplication::translate("MainWindow", "Backup wallet", Q_NULLPTR));
         m_autostartAction->setText(QApplication::translate("MainWindow", "Start on system login", Q_NULLPTR));
