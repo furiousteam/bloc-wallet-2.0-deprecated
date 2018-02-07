78204f1730244f09c37b325a9287a8d4e6b02321
furiousteam <36113405+furiousteam@users.noreply.github.com>
2018-02-07 05:25:29 +0100

    Update Settings.cpp

--- src/Settings/Settings.cpp
+++ src/Settings/Settings.cpp
@@ -64,7 +64,7 @@ const char OPTION_CLOSE_TO_TRAY[] = "closeToTray";
 const char OPTION_PRIVACY_PARAMS[] = "privacyParams";
 const char OPTION_PRIVACY_NEWS_ENABLED[] = "newsEnabled";
 
-const char DEFAULT_WALLET_FILE_NAME[] = "intensecoin.wallet";
+const char DEFAULT_WALLET_FILE_NAME[] = "blockchain-coin.wallet";
 const quint64 DEFAULT_OPTIMIZATION_PERIOD = 1000 * 60 * 30; // 30 minutes
 const quint64 DEFAULT_OPTIMIZATION_THRESHOLD = 10000000000000;
 const quint64 DEFAULT_OPTIMIZATION_MIXIN = 6;
